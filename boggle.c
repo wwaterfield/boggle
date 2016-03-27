1bafbaa1b616e978fecb5dadead360384fa7610a
Wyatt Waterfield <waterfield.wyatt@gmail.com>
2016-03-27 18:38:18 -0400

    Made createTrieNode function.

--- boggle.c
+++ boggle.c
@@ -11,3 +11,17 @@ int main(void)
 
 	return 0;
 }
+
+// Mallocs space and initializes a TrieNode.
+TrieNode *createTrieNode(void)
+{
+	int i;
+	TrieNode *newTrie = malloc(sizeof(TrieNode));
+
+	newTrie->isWord = 0;
+
+	for (i = 0; i < 26; i++)
+		newTrie->character[i] = NULL;
+
+	return newTrie;
+}
