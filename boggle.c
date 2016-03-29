737dfcb868972bc773d6e4fe93d7db9b9ad45519
Wyatt Waterfield <waterfield.wyatt@gmail.com>
2016-03-29 15:44:57 -0400

    Added dictionaries and functions for the trie.

--- boggle.c
+++ boggle.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#DEFINE STR_MIN 3
+#DEFINE STR_MAX 16
 
 typedef struct TrieNode {
 	int isWord;
 	struct TrieNode *character[26];
 } TrieNode;
 
+TrieNode *createTrieNode(void);
+TrieNode *insertString(TrieNode *root, char *string);
+
+
 int main(void)
 {
+    int numWords, i;
+    char temp[17];
+
+    FILE *ifp = fopen("dictionary.txt", "r")
+
+    fscanf(ifp, "%d",&numWords);
+
+    for (i = 0; i < numWords; i++)
+    {
+
+    }
+
+
 
 	return 0;
 }
@@ -24,4 +43,33 @@ TrieNode *createTrieNode(void)
 		newTrie->character[i] = NULL;
 
 	return newTrie;
+}
+
+TrieNode *insertString(TrieNode *root, char *string)
+{
+    int i, index;
+    int length = strlen(string);
+    TrieNode *temp;
+
+    if (root == NULL)
+        root = createTrieNode();
+
+    for (i = 0; i < length; i++)
+    {
+        index = tolower(string[i] - 'a');
+        temp = temp->children[index];
+    }
+
+    temp->isWord = 1;
+
+    return root;
+}
+
+void destroyTrie(TrieNode *root)
+{
+    int i;
+    if (root == NULL)
+        return;
+
+    for (i = 0; i < 26; i++)
 }
